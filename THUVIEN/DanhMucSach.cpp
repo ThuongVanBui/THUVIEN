6e3bf16147a30a68ae6494595016a3edfa862077
Day Van Bui <bvd2.2014@users.noreply.github.com>
2019-07-10 21:47:31 +0700

    no message

--- THUVIEN/DanhMucSach.cpp
+++ THUVIEN/DanhMucSach.cpp
@@ -1,6 +1,5 @@
 #include "mylib.h"
 
-char MENU_DMS[4][30] 		= {"1.Them    ","2.Xoa     ","3.Sua     ","4.Xem DS  " };
 
 enum TrangThaiDMS {
 	DUOCMUON = 0,
@@ -16,18 +15,18 @@ typedef struct DanhMucSach {
 };
 
 //Danh Sach SACH - DANH SACH LIEN DON
-typedef struct NodeDMS {
+typedef struct NodeDANHMUCSACH {
 	DanhMucSach dms;
-	NodeDMS *dmsNext;
+	NodeDANHMUCSACH *dmsNext;
 
 };
-typedef struct NodeDMS *NodeDMS_PTR;
+typedef struct NodeDANHMUCSACH *NodeDANHMUCSACHPTR;
 
-struct ListDMS{
-	NodeDMS_PTR dmsFirst;
-	NodeDMS_PTR dmsLast;
+struct ListDANHMUCSACH{
+	NodeDANHMUCSACHPTR dmsFirst;
+	NodeDANHMUCSACHPTR dmsLast;
 };
-typedef struct ListDMS LISTDMS;
+typedef struct ListDANHMUCSACH LISTDMS;
 
 
 void khoiTaoDS(LISTDMS &lsDMS){
@@ -39,8 +38,8 @@ int Rong(LISTDMS lsDMS){
 }
 
 void themDau(LISTDMS &lsDMS, DanhMucSach dms) {
-	NodeDMS_PTR p;
-	p = new NodeDMS;
+	NodeDANHMUCSACHPTR p;
+	p = new NodeDANHMUCSACH;
 	p->dms = dms;
 	p->dmsNext = NULL;
 	if (Rong(lsDMS) == 0) {
@@ -54,95 +53,35 @@ void themDau(LISTDMS &lsDMS, DanhMucSach dms) {
 }
 
 
-void NhapSach(LISTDMS &ls, char *s) {
-	int c;
-	int vitri;
+void NhapSach(LISTDMS &ls) {
 	while(true) {
 	DanhMucSach item;
-	clrscr();
 	printf("Nhap ma sach:");
-NHAPMA:
-	c = getch();
-	if ( ( c >= 'A' && c <= 'Z') || ( c >='a' && c <= 'z' ) ) {
-		scanf("%s",&item.MaSach);
-
-	} else {
-		goto NHAPMA;
- 
-		
-	}
-	
+	scanf("%s",&item.MaSach);
 	if (strcmp(item.MaSach,"0") == 0) {
 		break;
 	}
 	printf("Nhap trang thai(0:Duoc Muon, 1:Da Duoc Muon, 2:Da Thanh Ly): ");
-
-NHAPTT:
-	c = getch();
-	if ( ( c <= '9' && c >= '0') ) {
-		scanf("%d",&item.trangthaiDMS);
-
-	} else {
-		goto NHAPTT;
- 
-		
-	}
+	scanf("%d",&item.trangthaiDMS);
 	printf("Nhap vi tri:");
-NHAPVITRI:
-	if ( ( c >= 'A' && c <= 'Z') || ( c <= '9' && c >= '0') || ( c >='a' && c <= 'z' ) ) {
-		scanf("%s",&item.ViTri);
-
-	} else {
-		goto NHAPVITRI;
-
-	}
+	scanf("%s",&item.ViTri);
 	themDau(ls, item);
-	
 	};
 	
 }
 
 
 void XuatDMS(LISTDMS ls) {
-	for(NodeDMS_PTR p = ls.dmsFirst; p != NULL; p = p->dmsNext) {
+	for(NodeDANHMUCSACHPTR p = ls.dmsFirst; p != NULL; p = p->dmsNext) {
 		printf("Ma sach: %s", p->dms.MaSach);
 		printf("Trang thai: %d", p->dms.trangthaiDMS);
 		printf("Vi tri: %s\n", p->dms.ViTri);
 	}
 }
 
-
-void HienThiMenu(LISTDMS &ls) {
-	int choice;
-	char *s;
-	for (int i = 0; i < 4; i++) {
-		printf(MENU_DMS[i]);
-
-	}
-	do {
- 		scanf("%d",&choice);
-   
- 	switch (choice) {
-    case 1: 
-    		clrscr();
-    		NhapSach(ls, s);
-			break;
-    case 2:	printf(MENU_DMS[1]); 
-          break;
-    case 3: printf(MENU_DMS[2]); 
-         break;
-    case 4:printf(MENU_DMS[3]); 
-         break;
-     default: printf("Wrong Choice. Enter again\n");
-         break;
- 	} 
-  
-	} while (choice != 0);
-}
-
 int main() {
 	LISTDMS ls;
 	khoiTaoDS(ls);
-//	XuatDMS(ls);
-	HienThiMenu(ls);
+	NhapSach(ls);
+	XuatDMS(ls);
 }
